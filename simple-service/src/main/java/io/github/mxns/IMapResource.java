0851b4f042f9bf7ec23ac1a2de2f34081135f5c8
Måns Rolandi <mans@phenixid.se>
2017-10-15 13:41:19 +0200

    Initial commit

---
+++ simple-service/src/main/java/io/github/mxns/IMapResource.java
@@ -0,0 +1,40 @@
+package io.github.mxns;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.IMap;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+@Path("{mapId}")
+public class IMapResource {
+    private final IMap<String, String> data;
+
+    public IMapResource(@PathParam("mapId") String mapId) {
+        data = Hazelcast.getHazelcastInstanceByName("hazelcast-lab").getMap(mapId);
+    }
+
+    @POST
+    public Response post(@FormParam("key") String key, @FormParam("value") String value) {
+        data.put(key, value);
+        return Response.ok().build();
+    }
+
+    @GET
+    public Response get(@QueryParam("key") String key) {
+        String value = data.get(key);
+        if (value == null) {
+            return Response.status(404).build();
+        }
+        return Response.ok(value).build();
+    }
+
+    @DELETE
+    public Response delete(@QueryParam("key") String key) {
+        String value = data.remove(key);
+        if (value == null) {
+            return Response.status(404).build();
+        }
+        return Response.ok().build();
+    }
+}
---
+++ simple-service/src/main/java/io/github/mxns/Main.java
@@ -0,0 +1,24 @@
+package io.github.mxns;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.XmlConfigBuilder;
+import com.hazelcast.core.Hazelcast;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+
+public class Main {
+    static final String BASE_URI = "http://localhost:8080/maps/";
+
+    public static void main(String[] args) throws IOException {
+        InputStream url = Main.class.getClassLoader().getResourceAsStream("hazelcast.xml");
+        Config cfg = new XmlConfigBuilder(url).build();
+        Hazelcast.getOrCreateHazelcastInstance(cfg);
+        ResourceConfig rc = new ResourceConfig().packages("io.github.mxns");
+        GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+}
+
